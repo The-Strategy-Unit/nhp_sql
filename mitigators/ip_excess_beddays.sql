f7ee6ccf845c90a2650f7690066dfbf4111b5018
Tom Jemmett <thomas.jemmett@nhs.net>
2024-07-22 14:01:57 +0100

    fixes bug in excess beddays
    always used the "emergency" column for the trimpoint. the method used in production uses the correct columns

--- mitigators/ip_excess_beddays.sql
+++ mitigators/ip_excess_beddays.sql
@@ -5,10 +5,7 @@ CREATE VIEW nhp_strategies.ip_excess_beddays
 AS
 SELECT
   i.*,
-  CASE
-    WHEN i.ADMIMETH LIKE '1%' THEN 'excess_beddays_elective'
-    WHEN i.ADMIMETH LIKE '2%' THEN 'excess_beddays_emergency'
-  END [strategy],
+  'excess_beddays_elective' [strategy],
   1 [fraction]
 FROM
   nhp_modelling.inpatients i
@@ -17,7 +14,22 @@ INNER JOIN
   ON
     i.SUSHRG = t.sushrg
 WHERE
-  i.ADMIMETH LIKE '[12]%'
+  i.ADMIMETH LIKE '1%'
+AND
+  i.SPELDUR > t.elective
+UNION ALL
+SELECT
+  i.*,
+  'excess_beddays_emergency' [strategy],
+  1 [fraction]
+FROM
+  nhp_modelling.inpatients i
+INNER JOIN
+  nhp_modelling_reference.hrg_trimpoints t
+  ON
+    i.SUSHRG = t.sushrg
+WHERE
+  i.ADMIMETH LIKE '2%'
 AND
   i.SPELDUR > t.emergency
 ;
